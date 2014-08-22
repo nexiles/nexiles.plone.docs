3bd44b62b8a75073ac6ebb7e7cc01ac801047479
Jan Müller <jan.mueller@nexiles.com>
2014-08-22 16:37:12 +0200

    added upgrade step for capitalization

--- plone/docs/upgrades.py
+++ plone/docs/upgrades.py
@@ -22,41 +22,30 @@ def convert_docs(context, logger=None):
     setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
 
     catalog = api.portal.get_tool(name='portal_catalog')
-    brains = catalog(portal_type='docmeta')
+    brains = catalog(portal_type='plone.docs.docmeta')
 
     count = 0
     for brain in brains:
         source = brain.getObject()
+        data = {
+            "title": source.title,
+            "parent": source.getParentNode(),
+            "version": source.version,
+            "doc_url": source.doc_url,
+            "doc_icon": source.doc_icon,
+            "zip": source.zip,
+            "creators": source.Creator()
+        }
+        api.content.delete(obj = source)
         doc = api.content.create(
             type='plone.docs.docmeta',
-            title=source.title,
-            container=source.getParentNode())
-        doc.version = source.version
-        doc.url = source.url
-        doc.zip = source.zip
-        doc.icon = source.icon
-        doc.setCreators(source.Creator())
-        api.content.delete(obj = source)
+            title=data["title"],
+            container=data["parent"])
+        doc.version = data["version"]
+        doc.doc_url = data["doc_url"]
+        doc.zip = data["zip"]
+        doc.doc_icon = data["doc_icon"]
+        doc.setCreators(data["creators"])
         count += 1
 
     logger.info("%s objects converted." % count)
-
-def update_doc_folder(context, logger=None):
-    """Method to fix documentation folder
-
-    When called from the import_various method, 'context' is
-    the plone site and 'logger' is the portal_setup logger.
-
-    But this method will be used as upgrade step, in which case 'context'
-    will be portal_setup and 'logger' will be None.
-
-    """
-    if logger is None:
-        # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('plone.docs')
-
-    portal = api.portal.get()
-
-    docs = portal["documentation"]
-    docs.setLocallyAllowedTypes(("plone.docs.docmeta", "plone.docs.project"))
-    docs.setImmediatelyAddableTypes(("plone.docs.docmeta", "plone.docs.project"))
